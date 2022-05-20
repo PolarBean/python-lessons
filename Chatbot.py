3839b426914cd15240e9e749d15e52f427d7e240
Harry Carey <38996929+PolarBean@users.noreply.github.com>
2022-05-20 20:18:37 +0200

    added our lessons

---
+++ Chatbot.py
@@ -0,0 +1,7 @@
+def  my_function(argument1, argument2):
+    yourvariable = argument1*argument2
+    return yourvariable
+
+def Please_state_your_name(Name):
+    Hello_Name = "Hello " + Name
+    return Hello_Name
